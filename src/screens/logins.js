6928caf0a50c3babd2757ba4ac36f49befb61da7
pradip parmar <pradipparmar@pradips-MacBook-Pro.local>
2019-06-17 03:52:09 +0530

    app

---
+++ src/screens/logins.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Logins extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render(){
+        return(<div>
+            testing the Component
+            </div>
+            )
+    }
+}
+
+
+export default Logins
