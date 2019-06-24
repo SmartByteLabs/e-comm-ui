196cb324a93fafecbe1db2e7250bc02a6ca93923
pradip parmar <pradipparmar@pradips-MacBook-Pro.local>
2019-06-25 00:27:56 +0530

    screens copied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Login from './screens/logins'
+import Login from './screens/signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Login>Prince</Login>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login />
   );
 }
 
